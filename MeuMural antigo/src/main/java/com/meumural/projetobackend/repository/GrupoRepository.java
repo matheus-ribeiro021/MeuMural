df2dbaba1e108e009cce606134184886e25b5fd0
Matheus Ribeiro <matheusrsaraiva@gmail.com>
2026-05-28 19:23:39 -0300

    atualizando

--- MeuMural antigo/src/main/java/com/meumural/projetobackend/repository/GrupoRepository.java
+++ MeuMural antigo/src/main/java/com/meumural/projetobackend/repository/GrupoRepository.java
@@ -1,14 +1,16 @@
 package com.meumural.projetobackend.repository;
 
-import com.meumural.projetobackend.entity.Grupo;
-import jakarta.transaction.Transactional;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.meumural.projetobackend.entity.Grupo;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
